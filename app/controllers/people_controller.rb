e58086b0d9afcbd01336cb132cdb808b8cc835b4
Kunal Bhatia <kunalb@weboniselab.com>
2012-01-09 12:08:34 +0530

    before adding client_id to people

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -7,7 +7,7 @@ class PeopleController < ApplicationController
     Basecamp.establish_connection!('webonise.basecamphq.com','kunal.webonise','9930ghijk',true)
     @person=Basecamp::Person.all
     @person.each do |i|
-      @per=Person.new(:fist_name =>i.first_name, :last_name=> i.last_name,:email_address => i.email_address , :uuid=> i.uuid,:title=> i.title,:phone_number_mobile => i.phone_number_mobile)
+      @per=Person.new(:first_name =>i.first_name, :last_name=> i.last_name,:email_address => i.email_address , :uuid=> i.uuid,:title=> i.title,:phone_number_mobile => i.phone_number_mobile)
       @per.save
     end
   end
